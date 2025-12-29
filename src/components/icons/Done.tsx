00e7cf5fc3671a6306607e6f7d5d8148d53ab3bc
muhammadqamar <muhammadqamar111@gmail.com>
2025-12-29 18:34:10 +0500

    make a complete list component with all variants

--- src/components/icons/Done.tsx
+++ src/components/icons/Done.tsx
@@ -2,9 +2,10 @@ import type { SVGProps } from "react";
 
 type IconProps = SVGProps<SVGSVGElement> & {
   size?: number;
+  color?: string;
 };
 
-const SvgDone = ({ size = 24, ...props }: IconProps) => (
+const SvgDone = ({ size = 24, color = "#DCDCDC", ...props }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -15,8 +16,8 @@ const SvgDone = ({ size = 24, ...props }: IconProps) => (
   >
     <path
       d="M20 7L14.6093 13.4688C12.4732 16.0322 11.4051 17.3139 10 17.3139C8.59488 17.3139 7.52681 16.0322 5.39067 13.4688L4 11.8"
-      stroke="#DCDCDC"
-      strokeWidth="1.5"
+      stroke={color}
+      strokeWidth="2"
       strokeLinecap="round"
     />
   </svg>
--- src/components/icons/Info.tsx
+++ src/components/icons/Info.tsx
@@ -2,9 +2,10 @@ import type { SVGProps } from "react";
 
 type IconProps = SVGProps<SVGSVGElement> & {
   size?: number;
+  color?: string;
 };
 
-const SvgInfo = ({ size = 24, ...props }: IconProps) => (
+const SvgInfo = ({ size = 24, color = "#DCDCDC", ...props }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -15,7 +16,7 @@ const SvgInfo = ({ size = 24, ...props }: IconProps) => (
   >
     <path
       d="M12 11V17M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-      stroke="#DCDCDC"
+      stroke={color}
       strokeWidth="1.5"
       strokeLinecap="round"
     />
